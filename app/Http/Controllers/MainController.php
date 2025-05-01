82879797f2ef36b9ab9ea6d59c5f5282ac04e271
Márcio Junior <marciomsgj@gmail.com>
2025-05-01 15:17:57 -0300

    criando a página incial do projeto

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function home(){
-        echo 'Apresentar a página inicial';
+    public function home(): View{
+        return view('home');
     }
 
     public function generateExercises(Request $request){
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'home'])->name('home');
-Route::post('/generate-exercises', [MainController::class, 'generateExercises'])->name('generate-exercises');
-Route::get('/print-exercises', [MainController::class, 'printExercises'])->name('print-exercises');
-Route::get('/export-exercises', [MainController::class, 'exportExercises'])->name('export-exercises');
+Route::post('/generate-exercises', [MainController::class, 'generateExercises'])->name('generateExercises');
+Route::get('/print-exercises', [MainController::class, 'printExercises'])->name('printExercises');
+Route::get('/export-exercises', [MainController::class, 'exportExercises'])->name('exportExercises');
